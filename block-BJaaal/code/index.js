a52e2478e345bfe97a64d832cad885313923bd3a
Rahul kapoor <kapoorrahul304@gmail.com>
2022-10-19 13:16:13 +0530

    Template literal assign doen

--- block-BJaaal/code/index.js
+++ block-BJaaal/code/index.js
@@ -2,7 +2,9 @@
 
 /* Do the following:
 - Define a variable named `username` using `const` keyword and assign your name as a value.
+   const username="Rahul"
 - Define another variable named `bio` and assign a value `Learning to code`. Use `let` keyword to define the variable.
+     let bio="Learning to code"
 */
 
 let firstName = "Arya";
@@ -10,12 +12,18 @@ let lastName = "Stark";
 
 /* Do the following using `+` operator
   - Alert message saying `I am [username] and my bio is [bio]`. Replace square brackets with the value of variable.
+    alert(`userName ${bio}`);
+
   - Alert the message `I'm Arya Stark of Winterfell` using the variable name firstName and lastName.
+  Alert(`I` am [userName] ${bio}`);
 */
 
 /* Do the following using string template literal
   - Alert message saying `I am [username] and my bio is [bio]`. Replace square brackets with the value of variable.
+    Alert(`I` am [userName] ${bio}`);
+
   - Alert the message `I'm Arya Stark of Winterfell` using the variable name firstName and lastName.
+
 */
 
 let amount = 200;
@@ -23,8 +31,9 @@ const taxPercent = 10;
 
 /* Use string template literal:
   - Alert a message saying `The total amount is [amount]
+  Alert (total amount ${200}`);
     the tax amount is [(amount * taxPercent) / 100]
-  `
+  alert(200=)
   - Above message should be in two lines
 */
 
