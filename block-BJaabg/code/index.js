a4f1eef7b64e6e88bb4334ed3a2ad4512494caa2
Rahul kapoor <kapoorrahul304@gmail.com>
2022-10-28 15:32:55 +0530

    some improvment

--- block-BJaabg/code/index.js
+++ block-BJaabg/code/index.js
@@ -14,5 +14,7 @@ const ACCESSORY_PRICE = 9.99;
 var bank_balance = 303.91;
 var amount = 0;
 // your code goes here
+const = +prompt(`enter your amount`);
+
 
 // ⛑ Answer of the above will `$334.76`.
